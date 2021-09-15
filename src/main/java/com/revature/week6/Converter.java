8aec69ecd09fb32cbf3e4e5b3a340af15911891c
Juan Mendoza <juanmendoza0309@gmail.com>
2021-09-15 13:16:03 -0700

    finsihed decimal to octal converter

--- src/main/java/com/revature/week6/Converter.java
+++ src/main/java/com/revature/week6/Converter.java
@@ -3,13 +3,28 @@ package com.revature.week6;
 public class Converter {
 
     public static void main(String[] args){
+        Converter driver = new Converter();
 
+        String test1 = driver.octalConverter(394);
+        System.out.println(test1); // answer should be 612
+        System.out.println(driver.octalConverter(257));//should be 401
+        System.out.println(driver.octalConverter(520));//should be 1010
     }
 
-    public void rot13Encryption(String str){
-        str.toUpperCase();
-        System.out.print(str);
+    public String octalConverter(int num){
         StringBuffer sb = new StringBuffer();
 
+        while(num>=8){
+            int temp = num%8;
+            num = num/8;
+            sb.append(temp);
+            if(num<8){
+                temp = num%8;
+                sb.append(temp);
+            }
+        }
+        sb.reverse();
+        return sb.toString();
     }
+
 }
